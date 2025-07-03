2be9e48c2d095a4928a9560961cc5942cc1cff65
Canada Badiane <canada1555@hotmail.com>
2025-07-02 22:19:10 -0400

    Ajout des fonctions getArtistId et onViderFunction dans App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
+class App extends Component {
   state = { data: [], error: "" };
-  onChercher = async (a, b) => {
-    if (a && b) {
+
+  getArtistId = async (nomArtiste, choice, market) => {
+    const accessToken = "TON_TOKEN_ICI"; // remplace par ton token
+    if (nomArtiste) {
       try {
-        let reponse = await fetch(
-          `https://etablissements-publics.api.gouv.fr/v3/departements/${a}/${b}`
+        let response = await fetch(
+          `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+            nomArtiste
+          )}&type=artist`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
         );
-        let donnee = await reponse.json();
-        console.log(donnee);
-      } catch (e) {}
+        let donnee = await response.json();
+        this.setState({ data: donnee, error: "" });
+        const artist = donnee.artists.items[0];
+        const artistId = artist.id;
+
+        if (choice === "albums") {
+          let res = await fetch(
+            `https://api.spotify.com/v1/artists/${artistId}/albums`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+          let albumsDonnee = await res.json();
+          const albums = albumsDonnee.items;
+          this.setState({ albums, error: "" });
+        } else if (choice === "tracks") {
+          let res = await fetch(
+            `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=${market}`,
+            {
+              headers: { Authorization: `Bearer ${accessToken}` },
+            }
+          );
+          let topTracksDonnee = await res.json();
+          const topTracks = topTracksDonnee.tracks;
+          this.setState({ topTracks, error: "" });
+        }
+      } catch (e) {
+        this.setState({ error: "Connexion non aboutie avec l'Api" });
+      }
     } else {
+      this.setState({ error: "Veuillez écrire le nom d'un artiste" });
     }
   };
+
+  onViderFunction = () => {
+    this.setState({ data: [], error: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Spotify</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
